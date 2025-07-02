a7811cfc0048b4e2a94ff1b281dcfb1c894864c3
Miklós Kovács <fulcrum@kovacsmiki.com>
2025-07-02 19:39:44 +0900

    remove unecessary scripts

--- check_pkgbuilds.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -euo pipefail
-
-podman run --rm -it --userns keep-id \
--v $(pwd)/packages:/tmp/workdir:O \
--w /tmp/workdir \
-ghcr.io/mortyr45/archlinux:devtools \
-namcap -i */PKGBUILD
