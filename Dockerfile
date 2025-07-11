90b3dbc99cfa501bcc0f9f6c0ed8afeb53cc2e68
Miklós Kovács <fulcrum@kovacsmiki.com>
2025-07-11 10:10:13 +0900

    update build script and Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -4,14 +4,19 @@ RUN echo "ALL ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers
 RUN useradd --create-home user
 
 RUN pacman --noconfirm -Syu \
-    bat \
-    devtools \
-    git \
-    go \
-    nano \
-    namcap \
-    pacman-contrib \
-    rust
+  bat \
+  devtools \
+  git \
+  go \
+  nano \
+  namcap \
+  pacman-contrib \
+  rust
 RUN pacman-key --init
+
+COPY files/makepkg.conf /etc/makepkg.conf
+COPY files/build_package.sh /usr/bin/build_package
+
+RUN chmod +x /usr/bin/build_package
 
 USER user
--- files/build_package.sh
+++ files/build_package.sh
@@ -3,12 +3,12 @@
 set -euo pipefail
 
 fn_install_aur_package() {
-    set -euo pipefail
-    local temp_dir
-    temp_dir=$(mktemp -d)
-    git clone "https://aur.archlinux.org/$1.git" "$temp_dir"
-    cd "$temp_dir"
-    PKGDEST=/tmp/tmp_build makepkg --noconfirm --syncdeps --install
+  set -euo pipefail
+  local temp_dir
+  temp_dir=$(mktemp -d)
+  git clone "https://aur.archlinux.org/$1.git" "$temp_dir"
+  cd "$temp_dir"
+  PKGDEST=/tmp/tmp_build makepkg --noconfirm --syncdeps --install
 }
 
 fn_install_dependencies() {
@@ -36,17 +36,17 @@ fn_install_dependencies() {
 }
 
 fn_build_package() {
-    set -euo pipefail
-    cd "/tmp/workdir/$1"
-    local dependency_packages
-    dependency_packages=$(makepkg --printsrcinfo | { grep -e makedepends -e checkdepends || true; } | cut -d'=' -f2 | xargs)
-    [[ "$dependency_packages" != "" ]] && fn_install_dependencies "$dependency_packages"
-    cd "/tmp/workdir/$1"
-    makepkg --noconfirm --syncdeps --install
+  set -euo pipefail
+  cd "/tmp/workdir/$1"
+  local dependency_packages
+  dependency_packages=$(makepkg --printsrcinfo | { grep -e makedepends -e checkdepends || true; } | cut -d'=' -f2 | xargs)
+  [[ "$dependency_packages" != "" ]] && fn_install_dependencies "$dependency_packages"
+  cd "/tmp/workdir/$1"
+  makepkg --noconfirm --syncdeps --install
 }
 
 print_usage() {
-    printf "Usage: get_package_list.sh [options]
+  printf "Usage: get_package_list.sh [options]
 
     -l Build all packages in an order set by a .txt file.
     -p Build a specific package.
@@ -58,31 +58,31 @@ package_param=""
 
 [[ "$*" == "" ]] && print_usage && exit 1
 while getopts ":l:p:" opt; do
-    case ${opt} in
-        l ) list_param=$OPTARG;;
-        p ) package_param=$OPTARG;;
-        \? )
-            echo "Invalid option: -$OPTARG" 1>&2
-            exit 1
-            ;;
-        : )
-            echo "Option -$OPTARG requires an argument." 1>&2
-            exit 1
-            ;;
-    esac
+  case ${opt} in
+  l) list_param=$OPTARG ;;
+  p) package_param=$OPTARG ;;
+  \?)
+    echo "Invalid option: -$OPTARG" 1>&2
+    exit 1
+    ;;
+  :)
+    echo "Option -$OPTARG requires an argument." 1>&2
+    exit 1
+    ;;
+  esac
 done
-shift $((OPTIND -1))
+shift $((OPTIND - 1))
 
-mkdir ~/.gnupg && chmod 700 ~/.gnupg && echo "auto-key-retrieve" > ~/.gnupg/gpg.conf
+mkdir ~/.gnupg && chmod 700 ~/.gnupg && echo "auto-key-retrieve" >~/.gnupg/gpg.conf
 
 [[ "$list_param" != "" && "$package_param" != "" ]] && print_usage && exit 1
 
 if [[ "$list_param" != "" ]]; then
-    packages=$(cat /tmp/build_orders/$list_param.txt)
-    for package in $packages; do
-        fn_build_package $package
-    done
+  packages=$(cat /tmp/build_orders/$list_param.txt)
+  for package in $packages; do
+    fn_build_package $package
+  done
 fi
 if [[ "$package_param" != "" ]]; then
-    fn_build_package "$package_param"
+  fn_build_package "$package_param"
 fi
